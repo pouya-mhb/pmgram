70d3c35e86c4c0b7e470e809b6009d2957ad3900
Parnia Maroofi <parniamaroofi@gmail.com>
2021-06-05 18:45:08 +0430

    add vuetify to project

--- views/Welcome.vue
+++ views/Welcome.vue
@@ -1,5 +1,24 @@
 <template>
-    <div>
-        <h1>Welcome to PMgram</h1>
-    </div>
+  <div>
+    <h1>Welcome to PMgram</h1>
+    <br>
+    <v-btn @click="goToLoginPage()" color="primary" elevation="10">login</v-btn>
+  </div>
 </template>
+
+<script>
+import router from './../router'
+export default {
+    data() {
+        return {
+
+        }
+    },
+
+    methods:{
+        goToLoginPage(){
+            router.push("/login");
+        },
+    },
+}
+</script>
