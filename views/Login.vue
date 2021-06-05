13bfc6fa3771a073bb11018c6ed7cbfeabb1fb33
Parnia Maroofi <parniamaroofi@gmail.com>
2021-06-05 20:57:08 +0430

    add "vuetify icon"

--- views/Login.vue
+++ views/Login.vue
@@ -1,5 +1,14 @@
 <template>
-    <div>
-        <h1>Login</h1>
-    </div>
+  <div>
+    <h1>
+        <span>Login</span>
+        <v-icon large color="#2c3e50">mdi-login</v-icon>
+    </h1>
+  </div>
 </template>
+
+<script>
+export default {
+  
+}
+</script>
